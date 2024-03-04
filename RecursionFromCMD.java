624202b7fd893f1297af6180ca2b529d667168dd
CyrusRumao <cyrus.rumao006@gmail.com>
2024-03-04 22:25:22 +0530

    Name Change

--- RecursionFromCMD.java
+++ RecursionFromCMD.java
@@ -1,4 +1,4 @@
-public class Recursion {
+public class RecursionFromCMD {
     public static void main(String args[]) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
