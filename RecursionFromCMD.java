81068f6db2e1a049a50fb3a7252a68a1d742a43b
CyrusRumao <cyrus.rumao006@gmail.com>
2024-03-04 23:02:39 +0530

    .

--- RecursionFromCMD.java
+++ RecursionFromCMD.java
@@ -2,24 +2,13 @@ public class RecursionFromCMD {
     public static void main(String args[]) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        Power ob = new Power(a, b);
+        RecursionFromCMD ob = new RecursionFromCMD();
         int x = ob.pow(a, b);
         ob.display(x);
         // int power(int a, int b){
         // int x;
         // return power(x,b-1)
         // }
-    }
-}
-
-class Power {
-    int x = 0, y;
-    int r;
-
-    Power(int a, int b) {
-
-        x = a;
-        y = b;
     }
 
     int pow(int x, int y) {
